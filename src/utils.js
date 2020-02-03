34016c706a0ae240331c8be74c5bd84a7443ba4d
Sercan Paspals <sercanpaspal@gmail.com>
2020-02-03 20:57:24 +0300

    remove chunk files

--- src/utils.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randomInteger = function (min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; };
-//# sourceMappingURL=utils.js.map
